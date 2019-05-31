5891de635d46a4004d39675997995b50a37056a7
vfletes <vfletes@cusd.me>
2019-05-31 09:30:32 -0700

    eh

--- src/main/branch1.py
+++ src/main/branch1.py
@@ -1,3 +1,55 @@
 #methods/storylines here
 # import package src.main
 #help = https://docs.python-guide.org/writing/structure/
+
+location = 2.0 
+class castle():#(2,0) Emerald Jewel
+    response = input("You are now outside of the castle and see a shiny item under a stick")
+    if response.lower() in ['peer', 'peer']:
+        print("Obtained Emerald Jewel")
+        #add emerald Jewel to sql database
+class bridge():#(3,1) 
+
+class waterfall():#(2,1)
+
+class trail():#(4,1)
+
+class rustedCastle():#(5,0) Diamond Jewel
+
+class grassA():#(5,4)
+
+class grassB():#(5,6)
+
+class grassC():#(5,8)
+
+class miniCastle():#(4,8) Aquamarine Jewel / Key
+
+class tallCastle():#(4,9) Tanzanite Jewel
+
+class underbridge():#(3,3)
+
+class waterA():#(3,5)
+
+class waterB():#(3,6)
+
+class waterC():#(2,10) Sapphire Jewel
+
+class blueDoor():#(2,5) Peridot Jewel
+
+class darkCastle():#(2,6) Moonstone
+
+class trashTrail():#(2,8)
+
+class fork():#(1,8)
+
+class ladderA():#(1,9)
+
+class waterGrass():#(0,7)
+
+class mushroom():#(1,5) Topaz Jewel
+
+class windyCastle():#(1,11) Ruby Jewel
+
+class ladderB():#(0,10)
+
+class tallGrass():#(0,9) Onyx
